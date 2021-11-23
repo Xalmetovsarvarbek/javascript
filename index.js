7bbe9954a7ab7d27f61774f4ae81079df1705189
Xalmetovsarvarbek <XalmetovSarvarbek@gmail.com>
2021-11-23 17:04:29 +0500

    sonli amallarni bajaradihgan dastur

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 let a=5;
 let b=4;
-console.log(`${a}+${b}=${a+Math.sqrt(b)}`);
+console.log(`${a}+${b}=${a+b}`);
+console.log(`${a}-${b}=${a-b}`);
+console.log(`${a}*${b}=${a*b}`);
+console.log(`${a}/${b}=${a/b}`);
